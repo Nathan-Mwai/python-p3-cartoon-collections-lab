08f905ceba150e952aafd3b50c6189ea0c54c69d
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-30 08:50:44 +0300

    using for loops again but managed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
+def roll_call_dwarves(names):
+    [print(f"{index + 1}. {name}") for index,name in enumerate(names)]
     pass
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer):
+    return [f"{power.title()}!" for power in planeteer]
+    
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls )
+# any returns true or false if condition is met
 
-def find_the_cheese():
-    pass
+
+def find_the_cheese(snacks):
+    cheese_types = {"cheddar", "gouda", "camembert"}
+    
+    for snack in snacks:
+        if snack in cheese_types:
+            return snack
+    return None
+
